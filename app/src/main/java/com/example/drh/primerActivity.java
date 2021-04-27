8bd5d745af798209bdc704ae1b66f3d85ad56d88
Joan Roberto Garcia Garcia <joangarcia2015@gmail.com>
2021-04-27 10:07:53 -0500

    Commit test

--- app/src/main/java/com/example/drh/primerActivity.java
+++ app/src/main/java/com/example/drh/primerActivity.java
@@ -10,6 +10,7 @@ public class primerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primer);
+        //Comentario test
     }
 
 
