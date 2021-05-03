0f7e37e37ef471248097a71ad1c2272cbd91050c
carreonchis <82459780+carreonchis@users.noreply.github.com>
2021-05-02 23:58:20 -0500

    Cambios nuevos 1.1 Se agregó el método validarS() el cual nos permite checar el campo de sexo de la mascota. Se agregó el método validarUsuario() el cual nos permite checar el campo de usuario del propietario.

--- app/src/main/java/com/example/drh/crearP.java
+++ app/src/main/java/com/example/drh/crearP.java
@@ -29,13 +29,14 @@ public class crearP extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    String [] tipoUsuario= new String[]{"","ADMINISTRADOR","USUARIO"};
+    String[] tipoUsuario = new String[]{"", "ADMINISTRADOR", "USUARIO"};
     View vista;
     Button btnCrearP;
-    EditText et_Nombre, et_APater, et_AMater,et_Domicilio, et_Colonia,et_Ciudad,et_Estado
-            ,et_CP, et_Pais, et_TelFijo, et_TelCel, et_Correo, et_Contra, et_ConfContra, et_tipoUsuario;
-    String nombre, aPater, aMater, domicilio, colonia,ciudad, estado,cp,pais,telFijo,telCel,
-            correo,contra,confcontra,tUsuario;
+    EditText et_Nombre, et_APater, et_AMater, et_Domicilio, et_Colonia, et_Ciudad, et_Estado, et_CP,
+            et_Pais, et_TelFijo, et_TelCel, et_Correo, et_Contra, et_ConfContra, et_tipoUsuario;
+    String nombre, aPater, aMater, domicilio, colonia, ciudad, estado, cp, pais, telFijo, telCel,
+            correo, contra, confcontra, tUsuario;
+
     public crearP() {
         // Required empty public constructor
     }
@@ -67,59 +68,53 @@ public class crearP extends Fragment {
         }
 
 
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        vista= inflater.inflate(R.layout.fragment_crear_p, container, false);
+        vista = inflater.inflate(R.layout.fragment_crear_p, container, false);
         asignaciones(vista);
 
         return vista;
     }
-    public void asignaciones(View v){
+
+    public void asignaciones(View v) {
 
         //Asignaciones de EditText
-        et_Nombre= (EditText)v.findViewById(R.id.editNombre);
-        et_APater= (EditText)v.findViewById(R.id.editAppater);
-        et_AMater= (EditText)v.findViewById(R.id.editApMater);
-        et_Domicilio= (EditText)v.findViewById(R.id.editDomicilio);
-        et_Colonia= (EditText)v.findViewById(R.id.editColonia);
-        et_Ciudad= (EditText)v.findViewById(R.id.editCiudad);
-        et_Estado= (EditText)v.findViewById(R.id.editEstado);
-        et_CP= (EditText)v.findViewById(R.id.editCP);
-        et_Pais= (EditText)v.findViewById(R.id.editPais);
-        et_TelFijo= (EditText)v.findViewById(R.id.editTelFijo);
-        et_TelCel= (EditText)v.findViewById(R.id.editTelCelu);
-        et_Correo= (EditText)v.findViewById(R.id.editEmail);
-        et_Contra= (EditText)v.findViewById(R.id.editContra);
-        et_ConfContra= (EditText)v.findViewById(R.id.editConfContra);
-        btnCrearP= (Button)v.findViewById(R.id.btnCrearU);
-        et_tipoUsuario= (EditText)vista.findViewById(R.id.editTipoUsuario);
+        et_Nombre = (EditText) v.findViewById(R.id.editNombre);
+        et_APater = (EditText) v.findViewById(R.id.editAppater);
+        et_AMater = (EditText) v.findViewById(R.id.editApMater);
+        et_Domicilio = (EditText) v.findViewById(R.id.editDomicilio);
+        et_Colonia = (EditText) v.findViewById(R.id.editColonia);
+        et_Ciudad = (EditText) v.findViewById(R.id.editCiudad);
+        et_Estado = (EditText) v.findViewById(R.id.editEstado);
+        et_CP = (EditText) v.findViewById(R.id.editCP);
+        et_Pais = (EditText) v.findViewById(R.id.editPais);
+        et_TelFijo = (EditText) v.findViewById(R.id.editTelFijo);
+        et_TelCel = (EditText) v.findViewById(R.id.editTelCelu);
+        et_Correo = (EditText) v.findViewById(R.id.editEmail);
+        et_Contra = (EditText) v.findViewById(R.id.editContra);
+        et_ConfContra = (EditText) v.findViewById(R.id.editConfContra);
+        btnCrearP = (Button) v.findViewById(R.id.btnCrearU);
+        et_tipoUsuario = (EditText) vista.findViewById(R.id.editTipoUsuario);
 
 
+        btnCrearP.setOnClickListener(v1 -> {
+            obtenerDatos();
+            if (validar()) {
+                if (contra.equals(confcontra)) {
 
-       btnCrearP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                obtenerDatos();
-                if(validar()){
-                    if(contra.equals(confcontra)){
-
-                        //INSERTAR MÉTODO QUE COMPRUEBE EN LA BASE DE DATOS SI EL USUARIO YA EXISTE
-                        if(tUsuario==""){
-                            tUsuario="U";
-                        }
-                        Toast.makeText(vista.getContext(),"USUARIO CREADO",Toast.LENGTH_SHORT).show();
-                                vaciar();//DESPUÉS DE VALIDAR Y HABER INSERTADO LOS DATOS
-
-                    }else{
-                        Toast.makeText(vista.getContext(),"LAS CONTRASEÑAS NO COINCIDEN",Toast.LENGTH_SHORT).show();
+                    //INSERTAR MÉTODO QUE COMPRUEBE EN LA BASE DE DATOS SI EL USUARIO YA EXISTE
+                    if (tUsuario.equals("")) {
+                        tUsuario = "U";
                     }
-                }else{
-//REVISAR SPINNER PRIMERFRAGMENTO
+                    Toast.makeText(vista.getContext(), "USUARIO CREADO", Toast.LENGTH_SHORT).show();
+                    vaciar();//DESPUÉS DE VALIDAR Y HABER INSERTADO LOS DATOS
+
+                } else {
+                    Toast.makeText(vista.getContext(), "LAS CONTRASEÑAS NO COINCIDEN", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -148,33 +143,33 @@ public class crearP extends Fragment {
         } else if (confcontra.isEmpty()) {
             et_ConfContra.setError("EL CAMPO CONFIRMAR CONTRASEÑA NO PUEDE QUEDAR VACÍO");
             return false;
-        } else if(!tUsuario.equals("")||!tUsuario.equals("U")||!tUsuario.equals("A")){
-                et_tipoUsuario.setError("LLENE EL CAMPO CON U/A");
+        } else if (validarUsuario()) {
+            et_tipoUsuario.setError("LLENE EL CAMPO CON U/A");
             return false;
         }
         return true;
     }
 
-    public void obtenerDatos(){
+    public void obtenerDatos() {
 
-        nombre=et_Nombre.getText().toString();
-        aPater=et_APater.getText().toString();
-        aMater=et_AMater.getText().toString();
-        domicilio=et_Domicilio.getText().toString();
-        colonia=et_Colonia.getText().toString();
-        ciudad=et_Ciudad.getText().toString();
-        estado=et_Estado.getText().toString();
-        cp=et_CP.getText().toString();
-        pais=et_Pais.getText().toString();
-        telFijo=et_TelFijo.getText().toString();
-        telCel=et_TelCel.getText().toString();
-        correo=et_Correo.getText().toString();
-        contra=et_Contra.getText().toString();
-        confcontra=et_ConfContra.getText().toString();
-        tUsuario=et_tipoUsuario.getText().toString().trim();
+        nombre = et_Nombre.getText().toString();
+        aPater = et_APater.getText().toString();
+        aMater = et_AMater.getText().toString();
+        domicilio = et_Domicilio.getText().toString();
+        colonia = et_Colonia.getText().toString();
+        ciudad = et_Ciudad.getText().toString();
+        estado = et_Estado.getText().toString();
+        cp = et_CP.getText().toString();
+        pais = et_Pais.getText().toString();
+        telFijo = et_TelFijo.getText().toString();
+        telCel = et_TelCel.getText().toString();
+        correo = et_Correo.getText().toString();
+        contra = et_Contra.getText().toString();
+        confcontra = et_ConfContra.getText().toString();
+        tUsuario = et_tipoUsuario.getText().toString().trim();
     }
 
-    public void vaciar(){
+    public void vaciar() {
         et_Nombre.setText("");
         et_APater.setText("");
         et_AMater.setText("");
@@ -189,7 +184,18 @@ public class crearP extends Fragment {
         et_Correo.setText("");
         et_Contra.setText("");
         et_ConfContra.setText("");
+        et_tipoUsuario.setText("");
 
     }
 
+    public boolean validarUsuario() {
+        if (tUsuario.equals("")) {
+            return false;
+        } else if (tUsuario.equals("A")) {
+            return false;
+        } else if(tUsuario.equals("U")){
+            return false;
+        }
+        return true;
+    }
 }
